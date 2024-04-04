c2ab9cbdd8d4704a8a8e7cebb6074a74ba2c9584
Akorede <akoredeadewole8@gmail.com>
2024-04-04 14:57:38 +0100

    getNotification works

--- backend/tests/seed.ts
+++ backend/tests/seed.ts
@@ -1,7 +1,42 @@
 import Notifications from "../models/notifications";
-import { Request, Response, NextFunction } from "express";
+import logger from "../utils/logger";
+import connectDB from "../config/db";
+import {config} from "../config/config"
 
+const seedNotification = async () => {
 
-const seedNotification = async (req: Request, res: Response, next: NextFunction) => {
-    
+    try{
+        const feed = {
+            title: 'Welcome',
+            message: `With Carona, you get to share the ride with people going your way, enjoy a more 
+            comfortable commute, and maybe even make a few new friends along the way. Plus,
+            you're doing your part to take cars off the road, making Lagos a less chaotic city. 
+            We think that's pretty cool. `,
+            user: '660eab83b33e0419fde4e316'
+        }
+
+        const mongoUri = config.uri
+
+        await connectDB(mongoUri)
+
+        const newNotification = await Notifications.create({
+            title: feed.title,
+            message: feed.message,
+            user: feed.user
+        })
+        
+        if(!newNotification){
+            console.log(`Seeding failed`)
+        }
+
+        console.log(`Seeding successfully done`)
+
+    }catch(error){
+        logger.error(`Error`)
+        console.log(error)
+        console.log(`Could not seed Notification DB Finally`)
+    }
+
 }
+
+seedNotification()
--- backend/utils/notifications.ts
+++ backend/utils/notifications.ts
@@ -1,6 +1,3 @@
-import mongoose from "mongoose"
-
-
 
 interface NotificationTimestamp{
     createdAt: Date,
