eb5b3a536776197e7f14bf7ed8809e842071d849
Akorede <akoredeadewole8@gmail.com>
2024-05-27 11:26:06 +0100

    added latest updates

--- backend/app.ts
+++ backend/app.ts
@@ -5,13 +5,11 @@ import connectDB from './config/db'
 import router from './routes'
 import errorHandler from './middlewares/errorHandler'
 import notFound from './middlewares/notFound'
-import swaggerUI from 'swagger-ui-express'
-import YAML from 'yamljs'
+
 
 
 const app: Application = express()
 
-const swaggerDocument = YAML.load('./swagger.yaml')
 
 // set up
 const port = config.port
@@ -25,7 +23,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('<h1> Carona - A Carpooling Platform </h1>')
 })
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(`/api/v${apiVersion}`, router)
 
