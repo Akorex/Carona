b6788313d52ec2527edd476341162982ceb89d1d
Akorede <akoredeadewole8@gmail.com>
2024-05-30 15:32:26 +0100

    added more update to Trips

--- backend/controllers/trips.ts
+++ backend/controllers/trips.ts
@@ -14,6 +14,11 @@ export const createTrip = async (
     try{
         logger.info(`START: Create Trip Service`)
         const routeId = req.params.id
+        const userId = req.user.userId
+
+        // to do => ensure only signed-in users can createTrips
+        // to do => automatically update caronago trips with passengerId
+        // to do => automatically ensure caronashare trip creator is automatically in the passengerId 
 
         const {start, end, passengers, vehicleId} = req.body
         const tripData: {start: string; end: string; distance: string} = {start: '', end: '', distance: ''}
@@ -66,6 +71,8 @@ export const createTrip = async (
                 newTrip
             )
 
+        // payment service
+
         }
 
  catch(error){
@@ -79,5 +86,113 @@ export const getTrip = async (
     res: Response,
     next: NextFunction
 ) => {
-    
+    try{
+        logger.info(`START: Get Trip Service`)
+
+        const tripId = req.params.id
+        const userId = req.user.userId
+
+        const trip = await Trips.findOne({_id: tripId})
+
+        if (!trip){
+            logger.info(`END: Get Trip Service`)
+
+            return errorResponse(
+                res,
+                StatusCodes.NOT_FOUND,
+                `Trip not found`
+            )
+        }
+
+        logger.info(`END: Get Trip Service`)
+        successResponse(res,
+            StatusCodes.OK,
+            `Trip successfully fetched.`,
+            trip
+        )
+
+    }catch(error){
+        logger.error(`Could not get trip ${error}`)
+        next(error)
+    }
+}
+
+export const getAllTrips = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try{
+        // utility to get all trips by a user whether caronago or caronashare
+        logger.info(`START: Get All Trips Service`)
+        const userId = req.user.userId
+
+        const trips = await Trips.find({passengers: {$in: [userId]}})
+
+        if (!trips){
+            logger.info(`END: Get All Trips Service`)
+            return errorResponse(res,
+                StatusCodes.NOT_FOUND,
+                `No trips found for the passenger`
+            )
+
+        }
+
+        logger.info(`END: Get All Trips Service`)
+        successResponse(res,
+            StatusCodes.OK,
+            `Successfully fetched trips`,
+            trips
+        )
+
+    }catch(error){
+        logger.error(`Could not get All trips ${error}`)
+        next(error)
+    }
+
+}
+
+
+// restricted to CaronaShare Only => future work needed
+// todo: reduce availableSeats by amount of users added to Trip
+export const addPassengerToTrip = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try{
+        logger.info(`START: Add Passenger Service`)
+        const passengers = req.body
+        const tripId = req.params.id
+        const userId = req.user.userId
+
+        const trip = await Trips.findOneAndUpdate({_id: tripId, passengers: {$in: [userId]}}, 
+            {passengers: [userId, passengers]},
+            {runValidators: true, new: true}
+
+        )
+
+        if (!trip){
+            logger.info(`END: Add Passenger Service`)
+
+            return errorResponse(
+                res,
+                StatusCodes.BAD_REQUEST,
+                `Could not update Trip as Trip is invalid`
+            )
+        }
+
+
+        logger.info(`END: Add Passenger Service`)
+        successResponse(res,
+            StatusCodes.OK,
+            `Successfully added passengers to Trip`,
+            trip
+        )
+
+
+    }catch(error){
+        logger.error(`Could not update trip ${error}`)
+        next(error)
+    }
 }
--- backend/routes/trips.ts
+++ backend/routes/trips.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {isLoggedIn} from "../middlewares/authentication";
-import { createTrip } from "../controllers/trips";
+import { createTrip,
+    getTrip,
+    getAllTrips
+ } from "../controllers/trips";
 
 const tripsRouter = Router()
-tripsRouter.post('/:id', createTrip)
-tripsRouter.post('/', createTrip)
+tripsRouter.post('/:id', isLoggedIn, createTrip)
+tripsRouter.post('/', isLoggedIn, createTrip)
+tripsRouter.get('/:id', isLoggedIn, getTrip)
+tripsRouter.get('/', isLoggedIn, getAllTrips)
 
 export default tripsRouter
